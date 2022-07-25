bb046eeeae2c00a0c2c53f36f0631d41b854858f
Nagateja <valivetiteja2004@gmail.com>
2022-07-25 17:12:18 +0530

    Done Some THing

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Script Executed");
+
+let sidebar = document.querySelector('.sidebar');
+let toggle = document.querySelectorAll('button')[0];
+
+toggle.addEventListener('click', function(){
+    if (sidebar.style.marginLeft === "0px"){
+        sidebar.style.marginLeft = "-300px";
+        toggle.style.marginLeft = "-100px";
+        toggle.innerHTML = "Menu";
+    }
+    else{
+        sidebar.style.marginLeft = "0px";
+        toggle.style.marginLeft = "0px";
+        toggle.innerHTML = "Close";
+
+    }
+})
